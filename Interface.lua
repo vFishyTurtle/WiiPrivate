50a2b4b1d32b827c8a6b75ec5e1273905a054302
vFishyTurtle <123436512+vFishyTurtle@users.noreply.github.com>
2026-07-24 16:44:27 -0500

    Update Interface.lua

--- Interface.lua
+++ Interface.lua
@@ -351,7 +351,8 @@ function WiiUI:Window()
 		ShareGame.Visible 	= Enabled
 	end))
 
-	function Window:SendMessage(Color, Name, Message)
+	function Window:SendMessage(Color, Name, Message, TimeStamp)
+		local DT = DateTime.fromUnixTimestampMillis(TimeStamp)
 		local Message = createInstance("TextLabel", {
 			Name = "Message",
 			Position = UDim2.new(0.000, 0.000, 0.000, 0.000),
@@ -359,7 +360,7 @@ function WiiUI:Window()
 			Parent = ChatFrame,
 			BackgroundTransparency = 1,
 			FontFace = WiiUI.Fonts.ChatSemiBold,
-			Text = '<font color="#888888">[12:10] </font><font color="'..Color..'">'..Name..': </font>'..Message,
+			Text = '<font color="#888888">['..DT:FormatLocalTime("LT", "en-us")..'] </font><font color="'..Color..'">'..Name..': </font>'..Message,
 			AutomaticSize = Enum.AutomaticSize.XY,
 			TextXAlignment = Enum.TextXAlignment.Left,
 			TextYAlignment = Enum.TextYAlignment.Center,
